90fc8c5dfac0d4f2c5f6fd78c6e177116062f840
CodeLudovic <danielospinar@gmail.com>
2023-06-10 16:43:28 -0500

    PipesApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { MenuModule } from 'primeng/menu';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MenuModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
